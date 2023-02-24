c23126d08025e9a4f9ecc8f3a1c004f2bfe013a3
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-02-24 09:42:54 -0800

    implemented cors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-//
+const cors = require("cors");
 const routes = require("./src/routes");
 const app = express();
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const port = Number(process.env.PORT || 3333);
 
 async function connectDatabase() {
